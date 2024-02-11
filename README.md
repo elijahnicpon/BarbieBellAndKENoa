df777682ac11d58085c1b190cd7e6e447eb9cd88
Elijah Nicpon <elija@lawn-10-91-96-38.lawn.gatech.edu>
2024-02-11 12:58:48 -0500

    update home and add README

---
+++ README.md
@@ -0,0 +1,4 @@
+# BarbieBellAndKENoa
+
+BarbieBellAndKENoa is a project aimed at creating a dynamic duo who ask questions about your fitness goals to give custom workout and nutrition plans.
+This project was created using OpenAI, LangChain, MongoDB Atlas, Streamlit, and Python.
--- 🏠_home.py
+++ 🏠_home.py
@@ -6,26 +6,27 @@ st.markdown("#### A dynamic duo of elite personal trainer and nutritionist to he
 st.markdown("Hacklytics24 Submission by Elijah Nicpon, Julian Nogal, Emil Bajit, & Joshua Watkins")
 
 with st.expander("Your Information", expanded=True):
-    with st.form("Form"):
-        col1, col2, col3, col4 = st.columns(4)
-        with col1:
-            gender = st.selectbox("Gender", ['Select', 'Male', 'Female'])
-        with col2:
-            age = st.number_input("Age", step=1)
-        with col3:
-            weight = st.number_input("Weight (Lbs)", step=1)
-        with col4:
-            height = st.text_input("Height")
+    # with st.form("Form"):
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        gender = st.selectbox("Gender", ['Select', 'Male', 'Female'])
+    with col2:
+        age = st.number_input("Age", step=1)
+    with col3:
+        weight = st.number_input("Weight (Lbs)", step=1)
+    with col4:
+        height = st.text_input("Height")
 
-        # fitness_goals = st.text_area("What are your fitness goals TODO: examples!")
-        # exercise_experience = st.text_area("What exercise experience do you have? TODO: examples!")
-        # access_to_equipment = st.text_area("What sorts of exercise equipment do you have access to? TODO: examples!")
-        # time_commitment = st.text_area("How often are you available to exercise?")
-        preexisting_conditions = st.text_area("Do you have any preexisting medical conditions?")
-        additional_information = st.text_area("Any additonal information you would like to have considered?")
+    # fitness_goals = st.text_area("What are your fitness goals TODO: examples!")
+    # exercise_experience = st.text_area("What exercise experience do you have? TODO: examples!")
+    # access_to_equipment = st.text_area("What sorts of exercise equipment do you have access to? TODO: examples!")
+    # time_commitment = st.text_area("How often are you available to exercise?")
+    preexisting_conditions = st.text_area("Do you have any preexisting medical conditions?")
+    additional_information = st.text_area("Any additonal information you would like to have considered?")
 
-        submitted = st.form_submit_button("Submit")
-        if submitted:
+    col1,col2,fil = st.columns([1,1,4])
+    with col1:
+        if st.button("Chat with Barbie Bell"):
             st.session_state["health_metrics"] = {
                 "gender": gender,
                 "age": age,
@@ -39,3 +40,18 @@ with st.expander("Your Information", expanded=True):
                 "additional_info": additional_information
             }
             switch_page("Barbie Bell")
+    with col2:
+        if st.button("Chat with KENoa"):
+            st.session_state["health_metrics"] = {
+                "gender": gender,
+                "age": age,
+                "weight": weight,
+                "height": height,
+                # "fitness_goals": fitness_goals,
+                # "exercise_experience": exercise_experience,
+                # "access_to_equipment": access_to_equipment,
+                # "time_commitment": time_commitment,
+                "preexisting_conditions": preexisting_conditions,    
+                "additional_info": additional_information
+            }
+            switch_page("KENoa")
